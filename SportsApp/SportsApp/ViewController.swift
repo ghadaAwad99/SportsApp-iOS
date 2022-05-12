159834b2d17785b1998b643a5c965af205138923
Ghada Mustafa <74993384+ghadaAwad99@users.noreply.github.com>
2022-05-12 09:09:45 -0700

    first commit

---
+++ SportsApp/SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Hala Mustafa on 5/12/22.
+//  Copyright © 2022 Hala Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

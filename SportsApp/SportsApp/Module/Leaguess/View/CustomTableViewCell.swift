a6db9efb534d73f59e045a5be000811e08fcb9ce
NargesNagy <nargesnagy21@gmail.com>
2022-05-12 15:38:00 -0700

    nb
    nb

---
+++ SportsApp/SportsApp/Module/Leaguess/View/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  SportsApp
+//
+//  Created by Narges Nagy on 5/12/22.
+//  Copyright © 2022 Hala Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var cellLable: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellYoutubeButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
